785a817fee47bd0dd74d7eb8d69da56ca9cb5f81
kharboutmanel98 <kharbout2017@gmail.com>
2023-08-06 15:57:17 +0200

    Building up the logic

--- src/LoanForm.js
+++ src/LoanForm.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import { useState } from "react"
 import "./FormStyle.css"
-import Modal from './Modal'
+// import Modal from './Modal'
 
 const LoanForm = () => {
+    const [loanInputs, setLoanInputs] = useState({
+      name:"",
+      phoneNumber:"",
+      age:"",
+      isEmployee: false,
+      salaryRange: ""
+    });
+
+    function handleSubmit(event){
+      event.preventDefault();
+      alert("hello") 
+    }
+
+    const btnIsDisabled =
+    loanInputs.name == "" ||
+    loanInputs.age == "" ||
+    loanInputs.phoneNumber == "";
+
+    
+
   return (
     <div className="flex" style={{ flexDirection: "column"}}>
 
@@ -10,25 +30,34 @@ const LoanForm = () => {
       <h1>Requesting a Loan</h1>
         <hr />
         <label>Name:</label>
-        <input />
+        <input value={loanInputs.name} onChange={(e) => setLoanInputs({...loanInputs, name: e.target.value})}/>
 
         <label>Phone Number:</label>
-        <input />
+        <input value={loanInputs.phoneNumber} onChange={(e) => setLoanInputs({...loanInputs, phoneNumber: e.target.value})}/>
 
         <label>Age:</label>
-        <input />
+        <input value={loanInputs.age} onChange={(e) => setLoanInputs({...loanInputs, age: e.target.value})} />
 
         <label style={{marginTop:"30px"}}>Are You Employee?</label>
-        <input type="checkbox" />
+        <input type="checkbox" checked={loanInputs.checked} onChange={(e) => setLoanInputs({...loanInputs, isEmployee: e.target.checked})}/>
 
         <label>Salary</label>
-        <select>
+        <select value={loanInputs.salaryRange} onChange={(e) => setLoanInputs({...loanInputs, salaryRange: e.target.value})} >
             <option>Less Than 500$</option>
             <option>Between 500$ and 2000$</option>
             <option>Above 2000$</option>
         </select>
 
-        <button id="submit-loan-btn">Submit</button>
+        <button 
+           id="submit-loan-btn" 
+           className={btnIsDisabled ? "disabled" : "" }
+           onClick={handleSubmit}
+           disabled={
+            btnIsDisabled
+            }
+         >
+         Submit
+         </button>
         
       </form>
       {/* <Modal  /> */}
